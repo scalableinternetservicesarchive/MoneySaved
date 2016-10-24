39dcb7a5a0579748cc79951df5002938b1453e32
ChutianShen <sctstcjiayou@gmail.com>
2016-10-24 07:35:15 +0000

    Fix the database problem

--- db/migrate/20161017172536_add_index_to_likes.rb
+++ db/migrate/20161017172536_add_index_to_likes.rb
@@ -1,7 +1,8 @@
 class AddIndexToLikes < ActiveRecord::Migration[5.0]
   def change
-  	add_index :likes, :deal
-    add_index :likes, :user
-    add_index :likes, [:deal, :user], unique: true
+    
+#  	 add_index :likes, :deal
+#    add_index :likes, :user
+#    add_index :likes, [:deal, :user], unique: true
   end
 end
