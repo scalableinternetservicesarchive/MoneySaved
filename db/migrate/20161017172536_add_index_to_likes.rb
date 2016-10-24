b33314ea66f3eaa49aa0c0bb0593bb10ebbb4e57
diandians <qiufenggzhu@gmail.com>
2016-10-24 10:09:13 -0700

    fix likes_table problem

--- db/migrate/20161017172536_add_index_to_likes.rb
+++ db/migrate/20161017172536_add_index_to_likes.rb
@@ -1,14 +1,14 @@
 class AddIndexToLikes < ActiveRecord::Migration[5.0]
   def change
-<<<<<<< HEAD
+#<<<<<<< HEAD
     
-#  	 add_index :likes, :deal
-#    add_index :likes, :user
-#    add_index :likes, [:deal, :user], unique: true
-=======
-  	add_index :likes, :deal_id
-    add_index :likes, :user_id
-    add_index :likes, [:deal_id, :user_id], unique: true
->>>>>>> 20aa89e2f3cdb7873dc307150a34e0b58c8564ec
+  	 add_index :likes, :deal
+    add_index :likes, :user
+    add_index :likes, [:deal, :user], unique: true
+#=======
+ # 	add_index :likes, :deal_id
+ #   add_index :likes, :user_id
+ #   add_index :likes, [:deal_id, :user_id], unique: true
+#>>>>>>> 20aa89e2f3cdb7873dc307150a34e0b58c8564ec
   end
 end
