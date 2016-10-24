2cd45bc636e36c0892282812d30c25af2311544b
maston14 <maston14@hotmail.com>
2016-10-23 20:03:38 -0700

    fix margin problem

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -41,5 +41,16 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
-  
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
+
 end
