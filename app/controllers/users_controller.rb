81744b4956dcd0d054e399d4b6a2dce3dee2951b
ChutianShen <sctstcjiayou@gmail.com>
2016-10-24 17:12:40 +0000

    Improve User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,18 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+      if @user.update_attributes(user_params)
+      # 处理更新成功的情况
+    else
+      render 'edit'
+    end
+  end
 
   ########################################
   #above is action
