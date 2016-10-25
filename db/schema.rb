a7d7c2b06ec0e70c64a1da726fcf25d7a5bfce33
maston14 <maston14@hotmail.com>
2016-10-25 16:30:24 -0700

    add admin&block to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161024033707) do
+ActiveRecord::Schema.define(version: 20161025232853) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -78,10 +78,12 @@ ActiveRecord::Schema.define(version: 20161024033707) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin",           default: false
+    t.boolean  "blocked",         default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
