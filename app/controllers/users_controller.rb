ea9eeab43dae339c5f047d7a1ca2c0a40caa1032
diandians <qiufenggzhu@gmail.com>
2016-10-29 11:31:16 -0700

    finish admin index and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
   before_action :correct_user, only: [:edit, :update]
-  before_action :logged_in_user, only: [:edit, :update]
-  
+  before_action :logged_in_user, only: [:edit, :update, :destroy]
+  before_action :admin_user, only: [:index, :destroy]
   def new
     @user = User.new
     #debugger
   end
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
   
   def show
@@ -65,5 +65,13 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
-  
+    
+    def destroy
+      User.find(params[:id]).destroy
+      flash[:success] = "User deleted"
+      redirect_to users_url
+    end
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
