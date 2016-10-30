254c3f30414bda13eadd4d40e03611bc5dec5137
ChutianShen <sctstcjiayou@gmail.com>
2016-10-29 22:59:23 -0700

    done recommendation

--- withDB.py
+++ withDB.py
@@ -1,5 +1,15 @@
 import sqlite3
+from  datetime  import  *  
+import  time  
 
 conn = sqlite3.connect('db/development.sqlite3')
-
+#timestamp = time.time()
 print "Opened database successfully"
+
+
+conn.execute("INSERT INTO recommendations (id,user_id,deal_id, created_at, updated_at) \
+      VALUES (2, 4, 1, 1, 1)");
+
+conn.commit()
+print "Done recommendation"
+conn.close()
