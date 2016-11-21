00160508330e91240d0fe982380ec37b7a8a4fbd
maston14 <maston14@hotmail.com>
2016-11-20 23:35:43 -0800

    sql optimization

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,14 +5,14 @@ class CategoriesController < ApplicationController
   	
   	case params[:sort]
   		when "0"
-        @deal = Deal.where("label = ?",params[:id]).order(expdate: :desc).paginate(:page => params[:page], :per_page => 24)
+        @deal = Deal.includes(:likes).includes(:comments).where("label = ?",params[:id]).order(expdate: :desc).paginate(:page => params[:page], :per_page => 24)
   		when "1"
-  			@deal = Deal.where("label = ?",params[:id]).order(price: :desc).paginate(:page => params[:page], :per_page => 24)	
+  			@deal = Deal.includes(:likes).includes(:comments).where("label = ?",params[:id]).order(price: :desc).paginate(:page => params[:page], :per_page => 24)	
   		when "2"
-  			@deal = Deal.where("label = ?",params[:id]).order(price: :asc).paginate(:page => params[:page], :per_page => 24)	
+  			@deal = Deal.includes(:likes).includes(:comments).where("label = ?",params[:id]).order(price: :asc).paginate(:page => params[:page], :per_page => 24)	
   		else
         #@deal = ActiveRecord::Base.connection.execute('select * from deals inner join (SELECT "deal_id", count(*) as rev FROM "comments" GROUP BY "deal_id") temp ON "deals"."id" = "temp"."deal_id" where label = #{params[:id]}')
-  			@deal = Deal.where("label = ?",params[:id]).paginate(:page => params[:page], :per_page => 24)
+  			@deal = Deal.includes(:likes).includes(:comments).where("label = ?",params[:id]).paginate(:page => params[:page], :per_page => 24)
   	end
     fresh_when(@deal)
 
