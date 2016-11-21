65bbdc007b9c5fb798c847099e640c0c929408d5
ChutianShen <sctstcjiayou@gmail.com>
2016-11-20 20:59:58 -0800

    add foreigner gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161025232853) do
+ActiveRecord::Schema.define(version: 20161121044327) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
