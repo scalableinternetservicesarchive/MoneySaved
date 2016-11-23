e14ff4caee0eb864837dc713f027d7a9b3b978ee
maston14 <maston14@hotmail.com>
2016-11-23 13:39:19 -0800

    add server side cache

--- app/helpers/deals_helper.rb
+++ app/helpers/deals_helper.rb
@@ -1,2 +1,13 @@
 module DealsHelper
+
+	def cache_key_for_deal(deal)
+    	"deal/#{deal.id}/#{deal.updated_at}/#{deal.comments.count}#{deal.likes.count}"
+  	end
+
+  	def cache_key_for_deal_list(deals)
+    	ids = deals.map(&:id).join('-')
+    	max_updated_at = deals.map(&:id).max
+    	"deals/#{ids}-#{max_updated_at.to_i}"
+  	end
+
 end
